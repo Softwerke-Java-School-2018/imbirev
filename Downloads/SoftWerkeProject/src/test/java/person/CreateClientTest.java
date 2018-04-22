0855dd5f7425609bb81e6bf499dfcca543e6fed9
Nikolay Imbirev <imbirev.nikolay@gmail.com>
2018-04-22 22:50:43 +0300

    добавлены тесты к клиенту

--- Downloads/SoftWerkeProject/src/test/java/person/CreateClientTest.java
+++ Downloads/SoftWerkeProject/src/test/java/person/CreateClientTest.java
@@ -4,6 +4,7 @@ import com.nikolay.imbirev.connector.checker.Query;
 import com.nikolay.imbirev.connector.dbpackage.ClientDbService;
 import com.nikolay.imbirev.model.entities.Client;
 import com.nikolay.imbirev.model.entities.ClientTable;
+import com.nikolay.imbirev.view.ParsingClientData;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,12 +13,17 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class CreateClientTest {
 
     private final String name = "alex";
     private final String surname = "imbirev";
     private final LocalDate date = LocalDate.parse("30/06/1997", DateTimeFormatter.ofPattern("d/MM/yyyy"));
 
+
+    private static ParsingClientData clientData;
 
     private static ClientDbService service;
 
@@ -28,16 +34,58 @@ public class CreateClientTest {
 
     @Test
     public void getClient() {
-        service.sendToTable(new Client.ClientBuilder()
-                .setClientId()
-                .setFirstName(name)
-                .setLastName(surname)
-                .setDateofBirth(date)
-                .build());
-        Client c = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+        try {
+            clientData = new ParsingClientData("create table nikolay imbirev 30/06/1997");
+            clientData.parseCommand();
+            Client c = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+                    new Query(ClientTable.Cols.FIRST_NAME, name)
+            });
+        } catch (IllegalArgumentException e) {
+            assertNotEquals("", e.getMessage());
+        }
+
+        try {
+            clientData = new ParsingClientData("create client 3030 imbirev 30/06/1997");
+            clientData.parseCommand();
+            Client a = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+                    new Query(ClientTable.Cols.FIRST_NAME, name)
+            });
+        } catch (IllegalArgumentException e) {
+            assertNotEquals("", e.getMessage());
+        }
+        try {
+            clientData = new ParsingClientData("create nikolay imbirev 30/06/1997");
+            clientData.parseCommand();
+            Client a = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+                    new Query(ClientTable.Cols.FIRST_NAME, name)
+            });
+        } catch (IllegalArgumentException e) {
+            assertNotEquals("", e.getMessage());
+        }
+        try {
+            clientData = new ParsingClientData("client nikolay imbirev 30/06/1997");
+            clientData.parseCommand();
+            Client a = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+                    new Query(ClientTable.Cols.FIRST_NAME, name)
+            });
+        } catch (IllegalArgumentException e) {
+            assertNotEquals("", e.getMessage());
+        }
+        clientData = new ParsingClientData("create client nikolauskas imbiryov 30/06/1997");
+        clientData.parseCommand();
+        Client a = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
                 new Query(ClientTable.Cols.FIRST_NAME, name)
         });
-        Assert.assertEquals(name, c.getFirstName());
+        assertNotNull(a);
+        try {
+            clientData = new ParsingClientData(" client niko imbirev 30/06/1997");
+            clientData.parseCommand();
+            Client s = service.getFromTable(ClientTable.TABLE_NAME, new Query[]{
+                    new Query(ClientTable.Cols.FIRST_NAME, name)
+            });
+        } catch (IllegalArgumentException e) {
+            assertNotEquals("", e.getMessage());
+        }
     }
 
 }
