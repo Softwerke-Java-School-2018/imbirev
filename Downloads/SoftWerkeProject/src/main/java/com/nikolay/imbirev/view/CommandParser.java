b585af9d532e0a9f4f00dc627e28ed33daee83fb
Nikolay Imbirev <imbirev.nikolay@gmail.com>
2018-05-03 00:44:31 +0300

    начал парсинг комманд

--- Downloads/SoftWerkeProject/src/main/java/com/nikolay/imbirev/view/CommandParser.java
+++ Downloads/SoftWerkeProject/src/main/java/com/nikolay/imbirev/view/CommandParser.java
@@ -6,12 +6,24 @@ public class CommandParser implements CommandParserInterface {
 
     private final static Logger log = Logger.getAnonymousLogger();
 
+    private final static char startOfSearchConditions = '[';
+    private final static char endOfSearchConditions = ']';
+
+    private final static char startOfSortConditions = '{';
+    private final static char endOfSortConditions = '}';
+
+    private final static char delimiter = ',';
+
+    private final static char queryDelimiter = '=';
+
+    private final static char startOfInsertOrUpdate = '(';
+    private final static char endOfInsertOrUpdate = ')';
+    
+
     @Override
     public String parseCommand(String string) {
-        log.info("starting parse " + string);
-        String[] parts = string.split(" +");
-        for (String a : parts) {
-            log.info(a);
+        for (char letter : string.toCharArray()) {
+
         }
         return "good";
     }
