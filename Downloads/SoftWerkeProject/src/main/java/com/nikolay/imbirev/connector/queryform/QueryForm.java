a8b9bda259e84f6822870d26b39806e3e3531f48
Nikolay Imbirev <imbirev.nikolay@gmail.com>
2018-05-05 01:27:40 +0300

    полностью готова 2 этапная зашита от неправильного ввода пользователя, написаны комментарии и тесты, единственное что сейчас не отлавливается - дата, она будет обрабатываться в след классе поправлен функционал в результате тестирования

--- Downloads/SoftWerkeProject/src/main/java/com/nikolay/imbirev/connector/queryform/QueryForm.java
+++ Downloads/SoftWerkeProject/src/main/java/com/nikolay/imbirev/connector/queryform/QueryForm.java
@@ -46,18 +46,30 @@ public class QueryForm {
         return performOperation().toString();
     }
 
+    /**
+     * here we delegate continuous operation to each entity performer
+     * @return request of them operation
+     */
     private RequestCode performOperation() {
         switch (entity) {
             case "client":
-                return new ClientQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
+                return RequestCode.SUCCESS;
+                //return new ClientQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
             case "device":
-                return new DeviceQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
+                return RequestCode.SUCCESS;
+               // return new DeviceQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
             case "sale":
-                return new SaleQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
+                return RequestCode.SUCCESS;
+               // return new SaleQueryForm(operation, sortColumns, searchQueries, insertOrUpdateQueries).performOperation();
                 default: return RequestCode.DATA_ERROR;
         }
     }
 
+    /**
+     * here we check for avaliable conditions for each operation
+     * @param operation is a name of operation
+     * @return true if all conditions are good or false
+     */
     private boolean checkForAvailableOperation(@NotNull String operation) {
         switch (operation) {
             case "create":
@@ -81,18 +93,33 @@ public class QueryForm {
         }
     }
 
+    /**
+     * here we perform new array of columns if we want to sort query
+     * @return new array of columns or null (if initial array is null) or throw IllegalArgumentException
+     * if we have illegal columns here
+     */
     private Column[] getSortColumns() {
         if (sortArray == null) return null;
         Column[] resultArray = new Column[sortArray.length];
+        int counter = 0;
         for (int i = 0; i < sortArray.length; i++) {
             String queryName = sortArray[i].trim();
             for (String tableName : getAllTablesColumns()) {
-                if (queryName.equals(tableName))  resultArray[i] = Column.builder().columnName(queryName).build();
+                if (queryName.equals(tableName))  {
+                    if (queryName.contains("=,.:;")) throw new IllegalArgumentException();
+                    resultArray[i] = Column.builder().columnName(queryName).build();
+                    counter++;
+                }
             }
         }
+        if (counter == 0) throw new IllegalArgumentException();
         return resultArray;
     }
 
+    /**
+     * in this method we get all tables columns to check names of the query or sortArray
+     * @return string array of all columns
+     */
     private String[] getAllTablesColumns() {
         String[] resultArray = new String[SaleTable.Cols.columns.length + ClientTable.Cols.columns.length + DeviceTable.Cols.columns.length];
         int counter = 0;
@@ -113,15 +140,45 @@ public class QueryForm {
         return resultArray;
     }
 
+    /**
+     * here we perform query of arguments
+     * @param initialArray from this string array
+     * @return new query array or throw IllegalArgumentException (or null, if initialArray is empty)
+     */
     private Query[] getQuery(String[] initialArray) {
         if (initialArray == null) return null;
+        if (!checkForEquals(initialArray.length, initialArray)) throw new IllegalArgumentException();
         int num = 0;
+        int counter = 0;
         Query[] resultArray = new Query[initialArray.length];
         for (String item : initialArray) {
             String[] itemParts = item.split("=");
             if (num >= resultArray.length) throw new IllegalArgumentException();
-            resultArray[num++] = new Query(itemParts[0].trim(), itemParts[1].trim());
+            String nameColumnItem = itemParts[0].trim();
+            for (String colName : getAllTablesColumns()) {
+                if (nameColumnItem.equals(colName)) counter++; // if we have this column in entities -> increment counter
+            }
+            if (counter == 0) throw new IllegalArgumentException(); // we have illegal column
+            resultArray[num++] = new Query(nameColumnItem, itemParts[1].trim());
         }
         return resultArray;
+    }
+
+    /**
+     * here we check for illegal amount of '=' in the scope
+     * @param num is an illegal number
+     * @param array is a string array is needed to check
+     * @return true if okay or false if it is illegal
+     */
+    private boolean checkForEquals(int num, String[] array) {
+        int counter = 0;
+        for (String s : array) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '=') {
+                    counter++;
+                }
+            }
+        }
+        return counter == num;
     }
 }
